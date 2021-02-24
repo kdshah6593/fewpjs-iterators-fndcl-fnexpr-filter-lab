cad560cb70f94eec07934702637568a445c722c8
Kunal <kdshah6593@gmail.com>
2021-02-24 12:12:04 -0500

    commit tree 0efae5b6b79972d725c6ca76ca127264e58beb69

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(arr, str) {
+    let results = arr.filter(n => {
+        return n.toLowerCase() === str.toLowerCase();
+    });
+    return results
+}
+
+function fuzzyMatch(arr, str) {
+    let results = arr.filter(n => {
+        let l = str.length;
+        return n.slice(0, l) === str;
+    });
+    return results
+}
+
+function matchName(arr, str) {
+    let results = arr.filter(n => {
+        return n.name === str;
+    })
+    return results
+}
